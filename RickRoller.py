812b1819b4b70739ce8b04a1b2fe29e9edca85db
Holo-54 <officialholyman54@gmail.com>
2023-02-06 13:06:25 -0600

    feat: die choices, ping consequence

--- RickRoller.py
+++ RickRoller.py
@@ -3,6 +3,7 @@ import discord
 import random
 import asyncio
 from discord import app_commands
+from discord.app_commands import Choice
 
 # bot configuration
 intents = discord.Intents.all()
@@ -23,6 +24,21 @@ a = open('./API_Token.txt', 'r')
 api_token = a.read()
 a.close()
 
+# wrangler protocol users
+w = open('./Wrangler_Protocol_Users.txt','r')
+wranglers = w.readlines()
+w.close()
+
+# response list
+b = open('./Bot_Responses.txt','r')
+responses = b.readlines()
+bot_response = []
+for response in responses:
+    bot_response.append(response.strip('\n'))
+
+# types of dice
+die_sides = [4, 6, 8, 10, 12, 20, 100]
+
 # roll function
 def roll_dice(num_dice):
     roll = random.randint(1, num_dice)
@@ -39,40 +55,68 @@ def multi_roll(sides, num_dice):
         total_roll = total_roll + roll
     return rolls
 
-# response list
-b = open('./Bot_Responses.txt','r')
-responses = b.readlines()
-bot_response = []
-for response in responses:
-    bot_response.append(response.strip('\n'))
-
-# types of dice
-die_sides = [4, 6, 8, 10, 12, 20, 100]
-
 # -------------------------------------------------
-# EVENTS
+# EVENTS AND COMMANDS
 # -------------------------------------------------
 
 # basic roll
 @tree.command(name="roll", description="A basic dice roll")
 @app_commands.describe(die="Type of dice")
-async def roll(interaction: discord.Interaction, die: int):
-    if die in die_sides:
-        simple_roll = roll_dice(die)
-        await interaction.response.send_message(f'You rolled a {simple_roll}!')
-    else:
-        await interaction.response.send_message(f'"{die}" is not a valid dice!')
+@app_commands.choices(die=[
+    Choice(name=4, value=1),
+    Choice(name=6, value=2),
+    Choice(name=8, value=3),
+    Choice(name=10, value=4),
+    Choice(name=12, value=5),
+    Choice(name=20, value=6),
+    Choice(name=100, value=7)
+])
+async def roll(interaction: discord.Interaction, die: Choice[int]):
+    simple_roll = roll_dice(die.name)
+    await interaction.response.send_message(f'D{die.name}: You rolled **{simple_roll}**!')
 
 # multi roll
 @tree.command(name="multi_roll",description="Roll multiple dice")
 @app_commands.describe(die="Type of dice",number_of_dice="Number of dice")
-async def m_roll(interaction: discord.Interaction, die: int, number_of_dice: int):
-    mroll=multi_roll(die,number_of_dice)
-    await interaction.response.send_message(f'Here are your rolls:\n{mroll}\nGrand total: {total_roll}')
+@app_commands.choices(die=[
+    Choice(name=4, value=1),
+    Choice(name=6, value=2),
+    Choice(name=8, value=3),
+    Choice(name=10, value=4),
+    Choice(name=12, value=5),
+    Choice(name=20, value=6),
+    Choice(name=100, value=7)
+])
+async def m_roll(interaction: discord.Interaction, die: Choice[int], number_of_dice: int):
+    mroll=multi_roll(die.name,number_of_dice)
+    await interaction.response.send_message(f'You rolled: {number_of_dice} D{die.name}\nHere are your rolls:\n{mroll}\n**Grand total: {total_roll}**')
+
+# wrangler protocol -- WORK IN PROGRESS
+# @tree.command(name="wrangler_protocol",description="Annoy the living hell out of someone until they respond")
+# @app_commands.describe(poor_soul="@ of poor soul to be wrangled")
+# async def wrangler_protocol(interaction: discord.Interaction, poor_soul: discord.Member):
+#     if interaction.user in wranglers:
+#         await interaction.response.send_message('Commencing Protocol: Wrangle')
+#         while True:
+#             for i in range(5):
+#                 await interaction.channel.send(f'{poor_soul} This won\'t stop until someone says stop')
+#             def check(m):
+#                 return m.content.lower() == 'stop' and m.channel == interaction.channel
+#             try:
+#                 await client.wait_for('message', check=check, timeout=45)
+#                 await interaction.channel.send('oke')
+#                 break
+#             except asyncio.TimeoutError:
+#                 await interaction.channel.send('Still nothing...')
+#     else:
+#         await interaction.response.send_message(f'You aren\'t my superior! Only {interaction.user} is')
 
 @client.event
 async def on_message(message):
     print('{0.channel.id} Message from {0.author}: {0.content}'.format(message))
+    if client.user.mention in message.content.split(): # pinging the Lord consequence
+        reply = random.choice(bot_response)
+        await message.reply(reply)
 
 @client.event
 async def on_ready():
